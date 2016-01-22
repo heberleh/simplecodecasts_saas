02ab98e289d501afaf95b342391b28ac7e6cf1df
Henry Heberle <henryhbrl@gmail.com>
2016-01-22 17:25:01 -0200

    Problem: webpage/contacts/new does not work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122182458) do
+ActiveRecord::Schema.define(version: 20160122190556) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
